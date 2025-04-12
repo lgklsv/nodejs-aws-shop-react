de636adaa10efa46f832589213f504370d82000a
lgklsv <kolosov20011@gmail.com>
2025-04-12 18:25:57 +0400

    👽️ Replace api paths with new api gateway

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 const API_PATHS = {
   product: "https://pfwblnli9d.execute-api.us-east-1.amazonaws.com/prod",
   order:
-    "http://lgklsv-cart-api-prod.us-east-1.elasticbeanstalk.com/api/profile/cart",
+    "https://k1odwm4mp7.execute-api.us-east-1.amazonaws.com/api/profile/cart",
   import: "https://sc9u1qk4di.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "http://lgklsv-cart-api-prod.us-east-1.elasticbeanstalk.com/api",
+  cart: "https://k1odwm4mp7.execute-api.us-east-1.amazonaws.com/api",
 };
 
 export default API_PATHS;
